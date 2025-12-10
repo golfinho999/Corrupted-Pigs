7b104aebe1c61ce908faab09e2d02e3ea9999b4c
Golfinho Rebelde <ca@carlosafonso.com>
2025-12-10 23:37:25 +0000

    Add files via upload
    minor fix

--- COINK code..sol
+++ COINK code..sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.29;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract COINKtoken is ERC20 {
